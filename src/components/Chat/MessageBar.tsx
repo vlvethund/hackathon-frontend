6bfffabf3c127a9b0b4f1676b07d1216c3ac26c6
hund <utiful2004@gmail.com>
2024-03-23 19:38:11 +0900

    messageBar improved

--- src/components/Chat/MessageBar.tsx
+++ src/components/Chat/MessageBar.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Card, CardContent, Grid } from '@mui/material';
 
-const MessageBar: React.FC = () => {
-  return <></>;
+interface Props {
+  side?: 'left' | 'right';
+  text: string;
+}
+
+const MessageBar: React.FC<Props> = ({ side, text }) => {
+  const className = side === 'left' ? 'flex justify-end' : 'flex justify-start';
+  const color = side === 'left' ? 'white' : '#A4CE4E';
+
+  return (
+    <Grid className={className}>
+      <Card
+        sx={{ borderRadius: '40px', width: 'inherit', backgroundColor: color }}
+        className="mt-5 flex"
+      >
+        <CardContent>{text}</CardContent>
+      </Card>
+    </Grid>
+  );
 };
 
 export default MessageBar;
